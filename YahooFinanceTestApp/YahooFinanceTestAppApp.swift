3dc076d5edfaabca5a952b5e3e4c28f3f73357fa
Hoang Nguyen <hoang@hoang.llc>
2024-02-20 20:42:30 -0500

    Initial Commit

---
+++ YahooFinanceTestApp/YahooFinanceTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahooFinanceTestAppApp.swift
+//  YahooFinanceTestApp
+//
+//  Created by Hoang Nguyen on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct YahooFinanceTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

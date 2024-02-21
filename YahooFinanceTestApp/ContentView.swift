27c7e8ea5a59ba49f3f4626f80bb953bec07de35
Hoang Nguyen <hoang@hoang.llc>
2024-02-20 23:10:48 -0500

    Got basic call for yahoo finance

--- YahooFinanceTestApp/ContentView.swift
+++ YahooFinanceTestApp/ContentView.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
+import SwiftYFinance
 
 struct ContentView: View {
+    @State var textDisplay = "Hello Tien"
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(textDisplay)
         }
-        .padding()
+        .padding().onAppear(perform: {
+            
+            print("onAppear")
+            
+            SwiftYFinance.fetchSearchDataBy(searchTerm: "BYND", callback: {
+                
+                (result: [YFQuoteSearchResult ]?, error:  Error?) in
+                
+                if let someError = error {
+                    textDisplay = "Got error " + someError.localizedDescription
+                }else{
+                    
+                    if let someResult = result{
+                        if someResult.count > 0{
+                            
+                            textDisplay = "Got result for " +  (someResult[0].longname ?? "some long name")
+                            print("Got result back")
+                        }
+                       
+                    }
+          
+                }
+                
+            })
+        })
     }
 }
 
